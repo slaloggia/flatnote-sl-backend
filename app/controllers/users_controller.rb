3f2807aa2055149f5f1275475b11eb58feac4afb
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-01-02 16:57:07 -0600

    split create and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,12 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find_by(id: params[:id])
+        user = User.find_by(username: params[:username])
         render json: UserSerializer.new(user).to_serialized_json
     end
 
     def create
-        user = User.find_or_create_by(username: params[:username])
-        render json:UserSerializer.new(user).to_serialized_json
+        user = User.create(username: params[:username])
+        render json: UserSerializer.new(user).to_serialized_json
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :note_tags
   resources :notes
-  resources :users
+  resources :users, only: [:create, :index]
   resources :tags
+
+  get '/users/:username', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
